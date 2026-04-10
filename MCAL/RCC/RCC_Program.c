5b200a98120772586e6f24222c1cbb14b6a5aab2
Abdelrahman Gamal <agmal600@gmail.com>
2026-04-10 23:02:01 +0200

    Adding [ File Signature , LIBs and Module Files includes ]

---
+++ MCAL/RCC/RCC_Program.c
@@ -0,0 +1,14 @@
+/**************************************************************************************************************************** 
+*                                        Author: Abdelrahman Gamal OKASHA                                                   *
+*			                             Created: 10/4/2026                                                                 *
+*			                             File: RCC_Program.c                                                                *
+*			                             [ RCC Module Driver ]                                                              *
+****************************************************************************************************************************/
+
+#include "../../LIBs/STD_TYPES.h"
+#include "../../LIBs/BIT_MATH.h"
+#include "RCC_Interface.h"
+#include "RCC_Private.h"
+#include "RCC_Config.h"
+
+
